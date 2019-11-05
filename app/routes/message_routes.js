1c524ec842272f2f407fc9490144c635b40a92e0
asooge <asooge@gmail.com>
2019-11-05 09:58:19 -0500

    Created and tested curl scripts
    Created curl scripts for create, destroy, index, show, update.
    
    Tested create and show. Able to create and show 2 messages.
    
    updated routes from /message to /messages. updated server.js to
    recognize the routes.

--- app/routes/message_routes.js
+++ app/routes/message_routes.js
@@ -29,7 +29,7 @@ const router = express.Router()
 
 // INDEX
 // GET /message
-router.get('/message', requireToken, (req, res, next) => {
+router.get('/messages', requireToken, (req, res, next) => {
   Message.find()
     .then(message => {
       // `message` will be an array of Mongoose documents
@@ -45,7 +45,7 @@ router.get('/message', requireToken, (req, res, next) => {
 
 // SHOW
 // GET /message/5a7db6c74d55bc51bdf39793
-router.get('/message/:id', requireToken, (req, res, next) => {
+router.get('/messages/:id', requireToken, (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
   Message.findById(req.params.id)
     .then(handle404)
@@ -57,7 +57,7 @@ router.get('/message/:id', requireToken, (req, res, next) => {
 
 // CREATE
 // POST /message
-router.post('/message', requireToken, (req, res, next) => {
+router.post('/messages', requireToken, (req, res, next) => {
   // set owner of new message to be current user
   req.body.message.owner = req.user.id
 
@@ -74,7 +74,7 @@ router.post('/message', requireToken, (req, res, next) => {
 
 // UPDATE
 // PATCH /message/5a7db6c74d55bc51bdf39793
-router.patch('/message/:id', requireToken, removeBlanks, (req, res, next) => {
+router.patch('/messages/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
   delete req.body.message.owner
@@ -97,7 +97,7 @@ router.patch('/message/:id', requireToken, removeBlanks, (req, res, next) => {
 
 // DESTROY
 // DELETE /message/5a7db6c74d55bc51bdf39793
-router.delete('/message/:id', requireToken, (req, res, next) => {
+router.delete('/messages/:id', requireToken, (req, res, next) => {
   Message.findById(req.params.id)
     .then(handle404)
     .then(message => {
---
+++ curl-scripts/messages/create.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/messages"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "message": {
+      "text": "'"${TEXT}"'"
+    }
+  }'
+
+echo
